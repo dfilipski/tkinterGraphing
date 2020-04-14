0b896ee4279fc2bcf46c6c2a9377289382c374d6
ZorkySM <danny.filipski@gmail.com>
2020-04-14 10:58:37 -0400

    Add basic window

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import tkinter
+from tkinter import *
+
+class Window(Frame):
+
+    def __init__(self, master = None):
+        Frame.__init__(self, master)
+
+        self.master = master
+
+root = Tk()
+
+app = Window(root)
+
+root.mainloop()
