ada5b0d6b24cc15c939848eb3de5b45a10ab8440
ZorkySM <danny.filipski@gmail.com>
2020-04-19 20:22:21 -0400

    Add event listener for graph and very simple function evaluation

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import tkinter
 
+def getY1(int):
+    xRange = { #Make this its own function later
+        1: 0,
+        2: 0,
+        3: 0,
+    }
+    for x in xRange:
+        xRange[x] = int * x
+    return xRange
+
+def getX1():
+    return int(functionEntry.get())
+
+def graphFunction(event):
+    print(getY1(getX1()))
+
+
 root = tkinter.Tk()
 root.title("tkinterGraphing")
 root.geometry("640x640")
@@ -9,13 +26,17 @@ root.resizable(0, 0)
 inputFrame = tkinter.Frame(height = 60, width = 640)
 inputFrame.grid()
 
-functionLabel = tkinter.Label(inputFrame, text = "Y1")
+functionLabel = tkinter.Label(inputFrame, text = "Y1 = ")
 functionLabel.grid(row = 0)
 
-functionEntry = tkinter.Entry(inputFrame)
+functionEntry = tkinter.Entry(inputFrame, width = 2)
 functionEntry.grid(row = 0, column = 1)
 
+functionLabel = tkinter.Label(inputFrame, text = "x")
+functionLabel.grid(row = 0, column = 2)
+
 graphBtn = tkinter.Button(inputFrame, text="Graph")
-graphBtn.grid(row = 0, column = 2)
+graphBtn.grid(row = 2)
+graphBtn.bind("<Button-1>", graphFunction)
 
 root.mainloop()
